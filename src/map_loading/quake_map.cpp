7ee357e209f506df03a8ad181709fc1ec73c663f
MaximLeontev <maximricardo8@gmail.com>
2025-07-20 23:33:12 +0200

    added reading brushes from .map files

--- src/map_loading/quake_map.cpp
+++ src/map_loading/quake_map.cpp
@@ -1,4 +1,6 @@
 #include "quake_map.hpp"
+#include <array>
+#include <cassert>
 #include <cstdint>
 #include <cstring>
 #include <fstream>
@@ -11,6 +13,17 @@
 #include <vector>
 
 namespace {
+
+void skip_char(std::ifstream &file, char expected_char)
+{
+    char c;
+    file >> c;
+
+    if (c != expected_char) {
+        throw std::runtime_error(std::string("error: expected '") +
+                                 expected_char + "', got '" + c + "'");
+    }
+}
 
 // represents a key value pair in an entity
 class EntityInfo {
@@ -34,12 +47,81 @@ EntityInfo::EntityInfo(std::ifstream &file)
     this->value.erase(this->value.end() - 1);
 }
 
+// each plane of each brush is represented by a triangle (and some other info)
+class BrushPlane {
+
+public:
+    std::array<Vec3, 3> vs;
+    std::string texture;
+    Vec3 u, v;
+    float rotation;
+    Vec2 offset;
+    Vec2 scale;
+
+    explicit BrushPlane(std::ifstream &file);
+};
+
+BrushPlane::BrushPlane(std::ifstream &file)
+{
+    for (auto &v : this->vs) {
+        skip_char(file, '(');
+        file >> v.x;
+        file >> v.y;
+        file >> v.z;
+        skip_char(file, ')');
+    }
+
+    file >> this->texture;
+
+    skip_char(file, '[');
+    file >> this->u.x;
+    file >> this->u.y;
+    file >> this->u.z;
+    file >> this->offset.x;
+    skip_char(file, ']');
+
+    skip_char(file, '[');
+    file >> this->v.x;
+    file >> this->v.y;
+    file >> this->v.z;
+    file >> this->offset.y;
+    skip_char(file, ']');
+
+    file >> this->rotation;
+    file >> this->scale.x;
+    file >> this->scale.y;
+}
+
+class Brush {
+
+public:
+    std::vector<BrushPlane> planes;
+
+    // file should be pointing to the line after the left curly marking the
+    // start of the brush
+    Brush(std::ifstream &file);
+};
+
+Brush::Brush(std::ifstream &file)
+{
+    while (true) {
+        file >> std::ws;
+        if (file.peek() == '}')
+            break;
+
+        this->planes.emplace_back(file);
+    }
+
+    skip_char(file, '}');
+}
+
 class Entity {
 
 public:
     std::string name;
     // classname is not included in this list
     std::vector<EntityInfo> info;
+    std::vector<Brush> brushes;
 
     // file should be pointing to the line after the left curly marking the
     // start of the entity
@@ -55,14 +137,20 @@ Entity::Entity(std::ifstream &file)
         if (file.peek() == '}')
             break;
 
-        EntityInfo new_info(file);
-        this->info.push_back(new_info);
+        if (file.peek() == '{') {
+            skip_char(file, '{');
+            this->brushes.emplace_back(file);
+        } else {
+            this->info.emplace_back(file);
+        }
     }
+
+    skip_char(file, '}');
 
     size_t name_idx = this->get_info_idx("classname");
     if (name_idx == SIZE_MAX) {
         // have fun finding out where said entity is lol
-        throw std::runtime_error("error: entity missing a class name\n");
+        throw std::runtime_error("error: entity missing a class name");
     }
 
     this->name = this->info[name_idx].value;
@@ -87,19 +175,31 @@ std::vector<Triangle> read_file(std::ifstream &file)
 
     char c;
     while (true) {
+        file >> std::ws;
         file >> c;
         if (c == EOF || file.eof())
             break;
 
-        if (c == '{')
+        if (c == '}')
+            throw std::runtime_error("error: extraneous '}'");
+        else if (c == '{')
             entities.emplace_back(file);
     }
 
-    /*
     for (const auto &entity : entities) {
         std::cout << "found entity named " << entity.name << "\n";
+        for (const auto &brush : entity.brushes) {
+            std::cout << "new brush\n";
+            for (const auto &plane : brush.planes) {
+                std::cout << "new plane\n";
+                for (auto it = plane.vs.begin(); it < plane.vs.end(); ++it) {
+                    std::cout << "v" << std::distance(plane.vs.begin(), it)
+                              << " = (" << it->x << ", " << it->y << ", "
+                              << it->z << ")\n";
+                }
+            }
+        }
     }
-    */
 
     return {};
 }
